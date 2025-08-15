e4d8972050712cb2a20e170f5631dcdaa0ccc6e5
Dilli Basker <152502365+dillibasker@users.noreply.github.com>
2025-08-15 20:45:09 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
-    console.error(err.message);
+  
     process.exit(1);
   }
 };
